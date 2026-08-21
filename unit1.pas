951a84f70ddeb296a87f1b2a55c1765644650e0b
Denis Miroshnik <kanokito@gmail.com>
2026-08-21 13:41:02 +0300

    Доработана обработка GTIN

--- unit1.pas
+++ unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, StrUtils;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
 
@@ -22,7 +22,7 @@ type
     procedure FormCreate(Sender: TObject);
     procedure MemoInputChange(Sender: TObject);
   private
-
+    procedure ConvertLines;
   public
 
   end;
@@ -36,80 +36,83 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.ButtonConvertClickClick(Sender: TObject);
+procedure TForm1.ConvertLines;
 var
   i: Integer;
   InputLine, head, code, tail, FixedCode: string;
 begin
   MemoOutput.Clear;
 
+  if MemoInput.Lines.Count = 0 then Exit;
+
   for i := 0 to MemoInput.Lines.Count - 1 do
   begin
     InputLine := Trim(MemoInput.Lines[i]);
     if InputLine = '' then Continue;
 
-    // Разбиваем строку
     head := Copy(InputLine, 1, 19);
     code := Copy(InputLine, 20, 20);
-    tail := Copy(InputLine, 40, Length(InputLine) - 39); // Или Length(InputLine) - 40 + 1
-
-    // Заменяем только в code
+    tail := Copy(InputLine, 40, MaxInt);
+    //Добавляем служебные символы перед 91 и 92
     code := StringReplace(code, '91', #29 + '91', []);
     code := StringReplace(code, '92', #29 + '92', []);
 
-    // Собираем обратно
     FixedCode := head + code + tail;
-
     MemoOutput.Lines.Add(FixedCode);
   end;
+end;
+
+procedure TForm1.ButtonConvertClickClick(Sender: TObject);
+begin
+  ConvertLines;
+  ShowMessage('Конвертация выполнена!');
 end;
 
 procedure TForm1.ButtonExportCSVClickClick(Sender: TObject);
 var
   sl: TStringList;
   i: Integer;
-  Line, EscapedLine, FileName: string;
+  Line, EscapedLine: string;
 begin
-  if SaveDialog1.Execute then
+  // Автоматическая конвертация перед экспортом
+  ConvertLines;
+
+  if MemoOutput.Lines.Count = 0 then
   begin
-    sl := TStringList.Create;
-    try
-
-      ForceDirectories(ExtractFilePath(SaveDialog1.FileName));
-
-      for i := 0 to MemoOutput.Lines.Count - 1 do
-      begin
-        Line := Trim(MemoOutput.Lines[i]);
-        if Line = '' then Continue;
-
-        { --- Экранирование по RFC 4180 --- }
-        EscapedLine := StringReplace(Line, '"', '""', [rfReplaceAll]);
-        EscapedLine := '"' + EscapedLine + '"';
-
-        sl.Add(EscapedLine);
-      end;
-
-      // Сохраняем строго в UTF-8 без BOM
-      sl.SaveToFile(SaveDialog1.FileName, TEncoding.UTF8);
-      ShowMessage('Файл успешно сохранен: ' + ExtractFileName(SaveDialog1.FileName));
-
-    finally
-      sl.Free;
-    end;
+    ShowMessage('Нет данных для экспорта!');
+    Exit;
   end;
 
+  if not SaveDialog1.Execute then Exit;
+
+  sl := TStringList.Create;
+  try
+    ForceDirectories(ExtractFilePath(SaveDialog1.FileName));
+
+    for i := 0 to MemoOutput.Lines.Count - 1 do
+    begin
+      Line := Trim(MemoOutput.Lines[i]);
+      if Line = '' then Continue;
+
+      EscapedLine := StringReplace(Line, '"', '""', [rfReplaceAll]);
+      sl.Add('"' + EscapedLine + '"');
+    end;
+
+    sl.SaveToFile(SaveDialog1.FileName, TEncoding.UTF8);
+    ShowMessage('Файл успешно сохранен: ' + ExtractFileName(SaveDialog1.FileName));
+  finally
+    sl.Free;
+  end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-// Настройка диалога сохранения
   SaveDialog1.Filter := 'Файлы CSV|*.csv';
   SaveDialog1.DefaultExt := 'csv';
 
   MemoOutput.Clear;
   MemoInput.Clear;
-  
-    // Настройка формы
+
   BorderStyle := bsSingle;
   BorderIcons := BorderIcons - [biMaximize];
   Constraints.MinWidth := Width;
@@ -120,8 +123,8 @@ end;
 
 procedure TForm1.MemoInputChange(Sender: TObject);
 begin
-
+  // Очищаем результаты при изменении входных данных
+  MemoOutput.Clear;
 end;
 
 end.
-
